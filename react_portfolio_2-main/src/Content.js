b0c124afd062e2e21a730bbe898a46577f5bcf16
Abejide11 <garymiller82672@gmail.com>
2025-04-15 14:26:03 -0700

    my portfolio

---
+++ react_portfolio_2-main/src/Content.js
@@ -0,0 +1,252 @@
+// import images
+import Hero_person from "./assets/images/Hero/person.png";
+
+import figma from "./assets/images/Skills/figma.png";
+import sketch from "./assets/images/Skills/sketch.png";
+import ps from "./assets/images/Skills/ps.png";
+import reactjs from "./assets/images/Skills/react.png";
+import nodejs from "./assets/images/Skills/node.png";
+import python from "./assets/images/Skills/python.png";
+
+import services_logo1 from "./assets/images/Services/logo1.png";
+import services_logo2 from "./assets/images/Services/logo2.png";
+import services_logo3 from "./assets/images/Services/logo3.png";
+
+import project1 from "./assets/images/projects/img1.png";
+import project2 from "./assets/images/projects/img2.png";
+import project3 from "./assets/images/projects/img3.png";
+import person_project from "./assets/images/projects/person.png";
+
+import avatar1 from "./assets/images/Testimonials/avatar1.png";
+import avatar2 from "./assets/images/Testimonials/avatar2.png";
+import avatar3 from "./assets/images/Testimonials/avatar3.png";
+import avatar4 from "./assets/images/Testimonials/avatar4.png";
+
+import Hireme_person from "./assets/images/Hireme/person.png";
+import Hireme_person2 from "./assets/images/Hireme/person2.png";
+
+// import icons from react-icons
+import { GrMail } from "react-icons/gr";
+import { MdArrowForward, MdCall } from "react-icons/md";
+import { BsInstagram } from "react-icons/bs";
+import { TbSmartHome } from "react-icons/tb";
+import { BiUser } from "react-icons/bi";
+import { RiServiceLine, RiProjectorLine } from "react-icons/ri";
+import { MdOutlinePermContactCalendar } from "react-icons/md";
+
+export const content = {
+  nav: [
+    {
+      link: "#home",
+      icon: TbSmartHome,
+    },
+    {
+      link: "#skills",
+      icon: BiUser,
+    },
+    {
+      link: "#services",
+      icon: RiServiceLine,
+    },
+    {
+      link: "#projects",
+      icon: RiProjectorLine,
+    },
+    {
+      link: "#contact",
+      icon: MdOutlinePermContactCalendar,
+    },
+  ],
+  hero: {
+    title: "Web Developer",
+    firstName: "ABEJIDE",
+    LastName: "TIMILEYIN",
+    btnText: "Hire Me",
+    image: Hero_person,
+    hero_content: [
+      {
+        count: "2+",
+        text: "Years of Experinse in Web development",
+      },
+      {
+        count: "10+",
+        text: "Projects Worked in my career",
+      },
+    ],
+  },
+  skills: {
+    title: "Skills",
+    subtitle: "MY TOP SKILLS",
+    skills_content: [
+      {
+        name: "Figma",
+        para: "Designing user-friendly interfaces and creating seamless prototypes.",
+        logo: figma,
+        description: [
+          "Figma's collaborative features make it ideal for remote brainstorming sessions...",
+          "Figma can be used to design logos, icons, presentations...",
+          "Figma's component-based approach allows teams to create reusable design systems...",
+          "Figma's prototyping features enable interactive mockups and user flows..."
+        ]
+      },
+      {
+        name: "Node js",
+        para: "Building fast and scalable backend services using JavaScript",
+        logo: nodejs,
+        description: [
+          "Efficiently handles concurrent connections using event-driven architecture.",
+          "Ideal for building APIs, real-time applications, and microservices.",
+          "Wide support from NPM and a vibrant developer community.",
+          "Fast execution with the V8 engine and non-blocking I/O model."
+        ]
+      },
+      {
+        name: "Adobe Photoshop",
+        para: "Crafting high-quality visual assets and UI elements.",
+        logo: ps,
+        description: [
+          "Used for detailed image editing, retouching, and manipulation.",
+          "Great for creating UI mockups, social media graphics, and banners.",
+          "Supports advanced layer styles and effects for professional design.",
+          "Industry-standard software for visual content creation."
+        ]
+      },
+      {
+        name: "React js",
+        para: "Creating dynamic front-end experiences with reusable components.",
+        logo: reactjs,
+        description: [
+          "Builds dynamic UIs using reusable components.",
+          "Supports powerful features like hooks, context, and routing.",
+          "Works well with libraries like Redux and Tailwind CSS.",
+          "Backed by a large community and regular updates from Meta."
+        ]
+      },
+      {
+        name: "Sketch",
+        para: "LoDesigning sleek and modern UI/UX for mobile and web applications.",
+        logo: sketch,
+        description: [
+          "Mac-native design tool focused on vector UI design.",
+          "Popular for mobile-first designs and rapid prototyping.",
+          "Offers symbols and shared styles for consistency.",
+          "Used widely for iOS app design and wireframing."
+        ]
+      },
+      {
+        name: "Python",
+        para: "Developing scripts, web applications, and automations efficiently.",
+        logo: python,
+        description: [
+          "Clean syntax makes it ideal for beginners and professionals.",
+          "Used in web development, data science, machine learning, and scripting.",
+          "Vast ecosystem with libraries like Django, Pandas, and Flask.",
+          "Great for automation tasks and building quick prototypes."
+        ]
+  
+      },
+    ],
+    icon: MdArrowForward,
+  },
+  services: {
+    title: "Services",
+    subtitle: "WHAT I OFFER",
+    service_content: [
+      {
+        title: "Web Development",
+        para: "I build responsive and high-performing websites using modern technologies to deliver seamless user experiences and scalable solutions.",
+        logo: services_logo1,
+      },
+      {
+        title: "ui / ux DESIGNING",
+        para: "I craft intuitive, user-centered designs that combine aesthetics with functionality to improve engagement and usability across digital platforms.",
+        logo: services_logo2,
+      },
+      {
+        title: "PhotoShop Editing",
+        para: "I offer professional image editing services including retouching, color correction, and creative design to enhance and transform visual content.",
+        logo: services_logo3,
+      },
+    ],
+  },
+  Projects: {
+    title: "Projects",
+    subtitle: "MY CREATION",
+    image: person_project,
+    project_content: [
+      {
+        title: "Gym Website",
+        image: project1,
+      },
+      {
+        title: "Social Media web",
+        image: project2,
+      },
+      {
+        title: "Creative Website",
+        image: project3,
+      },
+    ],
+  },
+  Testimonials: {
+    title: "Testimonials",
+    subtitle: "MY CLIENT REVIEWS",
+    testimonials_content: [
+      {
+        review:
+          "“In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstra”",
+        img: avatar1,
+        name: "JOHN DOE",
+      },
+      {
+        review:
+          "“In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstra”",
+        img: avatar2,
+        name: "Tom Alex",
+      },
+      {
+        review:
+          "“In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstra”",
+        img: avatar3,
+        name: "Johnny",
+      },
+      {
+        review:
+          "“In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstra”",
+        img: avatar4,
+        name: "ROBBIN",
+      },
+    ],
+  },
+  Hireme: {
+    title: "Hire Me",
+    subtitle: "FOR YOUR PROJECTS",
+    image1: Hireme_person,
+    image2: Hireme_person2,
+    para: "Hiring me as your website developer means working with someone dedicated to understanding your business needs, providing personalized service, and delivering high-quality results. I offer a strong website, extensive experience, and a commitment to using the latest technologies and best practices.",
+
+
+    btnText: "Hire Me",
+  },
+  Contact: {
+    title: "Contect Me",
+    subtitle: "GET IN TOUCH",
+    
+    social_media: [
+      {
+        text: "abejidetimileyin54@gmail.com",
+        icon: GrMail,
+        link: "mailto:abejidetimileyin54@gmail.com",
+      },
+      {
+        text: "+234-810-787-0651",
+        icon: MdCall,
+        link: "https://wa.me/+2348107870651",
+      },
+   
+    ],
+  },
+  Footer: {
+    text: "All © Copy Right Reserved 2022",
+  },
+};
---
+++ react_portfolio_2-main/src/components/Projects.jsx
@@ -0,0 +1,31 @@
+import { content } from "../Content";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+import { Pagination } from "swiper";
+
+const Projects = () => {
+  const { Projects } = content;
+  return (
+    <section className="bg-bg_light_primary" id="projects">
+      <div className="md:container px-5 pt-14 min-h-screen flex flex-col justify-between">
+        <div>
+          <h2 className="title" data-aos="fade-down">
+            {Projects.title}
+          </h2>
+          <h4 className="subtitle" data-aos="fade-down">
+            {Projects.subtitle}
+          </h4>
+          <br />
+        </div>
+       
+      </div>
+    </section>
+  );
+};
+
+export default Projects;
